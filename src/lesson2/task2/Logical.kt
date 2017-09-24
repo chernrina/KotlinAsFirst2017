b8c27b8a772b65fddb5a6bf8f79c0dbd58f7a228
Chernikova Arina <arina657@mail.ru>
2017-09-24 18:21:51 +0300

    Решены задачи lesson2.task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
@@ -17,7 +18,12 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    val sum1 = number % 10 + (number / 10) % 10
+    val sum2 = (number / 100) % 10 + number / 1000
+    if (sum1 == sum2) return true
+    else return false
+}
 
 /**
  * Простая
@@ -26,7 +32,12 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return when {
+        x1 == x2 || y1 == y2 || Math.abs(x2 - x1) == Math.abs(y2 - y1) -> true
+        else -> false
+    }
+}
 
 /**
  * Средняя
@@ -36,7 +47,12 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    return when {
+        sqr(x2 - x1) + sqr(y2 - y1) <= sqr(r2) && Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) <= r2 - r1 -> true
+        else -> false
+    }
+}
 
 /**
  * Средняя
@@ -47,4 +63,10 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return when {
+        (a <= r && b <= s) || (a <= r && c <= s) || (b <= r && a <= s) || (b <= r && c <= s) ||
+                (c <= r && a <= s) || (c <= r && b <= s) -> true
+        else -> false
+    }
+}
